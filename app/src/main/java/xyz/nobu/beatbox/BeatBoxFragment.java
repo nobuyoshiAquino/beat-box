f231071bfc3c238def3c4aea4b955813ef224686
Nobuyoshi Aquino <aquinoishii@gmail.com>
2017-03-17 23:48:10 -0300

    Show a list of sound samples

---
+++ app/src/main/java/xyz/nobu/beatbox/BeatBoxFragment.java
@@ -0,0 +1,91 @@
+package xyz.nobu.beatbox;
+
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import xyz.nobu.beatbox.databinding.FragmentBeatBoxBinding;
+import xyz.nobu.beatbox.databinding.ListItemSoundBinding;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BeatBoxFragment extends Fragment {
+
+    private BeatBox mBeatBox;
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        FragmentBeatBoxBinding binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_beat_box, container, false);
+
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
+
+        return binding.getRoot();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private ListItemSoundBinding mBinding;
+
+        public SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+            mBinding.setViewModel(new SoundViewModel(mBeatBox));
+        }
+
+        public void bind(Sound sound) {
+            mBinding.getViewModel().setSound(sound);
+            mBinding.executePendingBindings();
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds) {
+            mSounds = sounds;
+        }
+
+        @Override
+        public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            ListItemSoundBinding binding = DataBindingUtil
+                    .inflate(inflater, R.layout.list_item_sound, parent, false);
+            return new SoundHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(SoundHolder holder, int position) {
+            Sound sound = mSounds.get(position);
+            holder.bind(sound);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mSounds.size();
+        }
+    }
+}
---
+++ app/src/main/java/xyz/nobu/beatbox/SoundViewModel.java
@@ -0,0 +1,31 @@
+package xyz.nobu.beatbox;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+/**
+ * Created by nobu on 3/17/17.
+ */
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+    }
+}
